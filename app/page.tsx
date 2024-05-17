3537db852eeb5774bfdbd1c53f37e80e96cdf6dd
feyzoOffice <feyzooffice@gmail.com>
2024-05-17 15:37:42 +0300

    Supabase setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import { supabase } from "@/lib/supabase";
+
 export default function Home() {
+  const setNewView = async () => {
+    const { data, error } = await supabase.from("views").insert({
+      name: "Random name",
+    });
+    if (data) console.log(data);
+    if (error) console.log(error);
+  };
+  setNewView();
   return (
     <div className="flex flex-col items-center">
       <h1>الرئيسية</h1>
