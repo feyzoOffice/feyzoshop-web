7f83dba6efba3528c8688f3f70f326a20fa4c045
feyzoOffice <feyzooffice@gmail.com>
2024-06-19 18:47:49 +0300

    Work on filters

--- components/products-filters.tsx
+++ components/products-filters.tsx
@@ -21,6 +21,7 @@ import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
 import {
   addFilter,
   deleteFilter,
+  resetFilters,
 } from "@/lib/redux/slices/filters/filters-slice";
 import { Badge } from "./ui/badge";
 import { useGetCategoriesQuery } from "@/lib/redux/slices/categories/categories-slice";
@@ -29,6 +30,7 @@ import { useGetSizesQuery } from "@/lib/redux/slices/sizes/sizes-slice";
 import { useGetColorsQuery } from "@/lib/redux/slices/colors/colors-slice";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Checkbox } from "./ui/checkbox";
+import { FormEventHandler } from "react";
 
 export function ProductsFilters({
   createQueryString,
@@ -72,37 +74,20 @@ const FilterDrawer = ({
   createQueryString: (name: string, value: string) => string;
   deleteQueryString: (name: string, value: string) => string;
 }) => {
-  const {
-    data: categories,
-    isLoading: isLoadingCategories,
-    isError: isErrorCategories,
-  } = useGetCategoriesQuery({});
-  const {
-    data: classes,
-    isLoading: isLoadingClasses,
-    isError: isErrorClasses,
-  } = useGetClassesQuery({});
-  const {
-    data: sizes,
-    isLoading: isLoadingSizes,
-    isError: isErrorSizes,
-  } = useGetSizesQuery({});
-  const {
-    data: colors,
-    isLoading: isLoadingColors,
-    isError: isErrorColors,
-  } = useGetColorsQuery({});
   const state = useAppSelector((store) => store.filters);
   const dispatch = useAppDispatch();
   const router = useRouter();
   const pathname = usePathname();
+
+  const filters = state.filters.filter((filter) => filter.value !== "all");
+
   return (
     <Drawer direction="right">
       <DrawerTrigger asChild>
         <Button variant="outline" size="icon">
-          {state.filters.length > 0 && (
+          {filters.length > 0 && (
             <Badge className="relative -ml-2">
-              {state.filters.length > 9 ? "+9" : state.filters.length}
+              {filters.length > 9 ? "+9" : filters.length}
             </Badge>
           )}
           <Filter />
@@ -114,113 +99,16 @@ const FilterDrawer = ({
             <Button
               variant="outline"
               onClick={() => {
+                dispatch(resetFilters(state.filters));
                 router.push(pathname);
               }}
             >
               جميع المنتجات
             </Button>
-            <Card>
-              <CardHeader>
-                <CardTitle>الفئات</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <RadioGroup defaultValue="all" dir="rtl">
-                  <div className="flex items-center gap-3 space-x-2">
-                    <RadioGroupItem value="all" id="all" />
-                    <Label htmlFor="all">الجميع</Label>
-                  </div>
-                  {(classes !== null || classes !== undefined) &&
-                    classes?.map((cla) => (
-                      <div
-                        className="flex items-center gap-3 space-x-2"
-                        key={cla.id}
-                      >
-                        <RadioGroupItem
-                          value={String(cla.title)}
-                          id={String(cla.title)}
-                        />
-                        <Label htmlFor={String(cla.title)}>{cla.title}</Label>
-                      </div>
-                    ))}
-                </RadioGroup>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>التصنيفات</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <RadioGroup defaultValue="all" dir="rtl">
-                  <div className="flex items-center gap-3 space-x-2">
-                    <RadioGroupItem value="all" id="all" />
-                    <Label htmlFor="all">الجميع</Label>
-                  </div>
-                  {(categories !== null || categories !== undefined) &&
-                    categories?.map((category) => (
-                      <div
-                        className="flex items-center gap-3 space-x-2"
-                        key={category.id}
-                      >
-                        <RadioGroupItem
-                          value={String(category.title)}
-                          id={String(category.title)}
-                        />
-                        <Label htmlFor={String(category.title)}>
-                          {category.title}
-                        </Label>
-                      </div>
-                    ))}
-                </RadioGroup>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>القياس</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-2">
-                  {(sizes !== null || sizes !== undefined) &&
-                    sizes?.map((size) => (
-                      <div
-                        className="flex items-center gap-3 space-x-2"
-                        key={size.id}
-                      >
-                        <Checkbox id={String(size.size)} />
-                        <label
-                          htmlFor={String(size.size)}
-                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                          {size.size}
-                        </label>
-                      </div>
-                    ))}
-                </div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>اللون</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-2">
-                  {(colors !== null || colors !== undefined) &&
-                    colors?.map((color) => (
-                      <div
-                        className="flex items-center gap-3 space-x-2"
-                        key={color.id}
-                      >
-                        <Checkbox id={String(color.title)} />
-                        <label
-                          htmlFor={String(color.title)}
-                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                          {color.title}
-                        </label>
-                      </div>
-                    ))}
-                </div>
-              </CardContent>
-            </Card>
+            <Classes />
+            <Categories />
+            <Sizes />
+            <Colors />
           </div>
         </ScrollArea>
 
@@ -231,6 +119,210 @@ const FilterDrawer = ({
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
+  );
+};
+
+const Categories = () => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((store) => store.filters.filters);
+  const category = state.find((filter) => filter.name === "category");
+  const {
+    data: categories,
+    isLoading: isLoadingCategories,
+    isError: isErrorCategories,
+  } = useGetCategoriesQuery({});
+  const handleChange = (value: string) => {
+    dispatch(addFilter({ name: "category", value: String(value) }));
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>التصنيفات</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <RadioGroup
+          dir="rtl"
+          value={category?.value}
+          defaultValue={category?.value}
+          onValueChange={handleChange}
+        >
+          <div className="flex items-center gap-3 space-x-2">
+            <RadioGroupItem value="all" id="all" />
+            <Label htmlFor="all">الجميع</Label>
+          </div>
+          {(categories !== null || categories !== undefined) &&
+            categories?.map((category) => (
+              <div
+                className="flex items-center gap-3 space-x-2"
+                key={category.id}
+              >
+                <RadioGroupItem
+                  value={String(category.id)}
+                  id={String(category.title)}
+                />
+                <Label htmlFor={String(category.title)}>{category.title}</Label>
+              </div>
+            ))}
+        </RadioGroup>
+      </CardContent>
+    </Card>
+  );
+};
+
+const Classes = () => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((store) => store.filters.filters);
+  const cla = state.find((filter) => filter.name === "class");
+  const {
+    data: classes,
+    isLoading: isLoadingClasses,
+    isError: isErrorClasses,
+  } = useGetClassesQuery({});
+  const handleChange = (value: string) => {
+    dispatch(addFilter({ name: "class", value: String(value) }));
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>الفئات</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <RadioGroup
+          dir="rtl"
+          value={cla?.value}
+          defaultValue={cla?.value}
+          onValueChange={handleChange}
+        >
+          <div className="flex items-center gap-3 space-x-2">
+            <RadioGroupItem value="all" id="all" />
+            <Label htmlFor="all">الجميع</Label>
+          </div>
+          {(classes !== null || classes !== undefined) &&
+            classes?.map((cla) => (
+              <div className="flex items-center gap-3 space-x-2" key={cla.id}>
+                <RadioGroupItem value={String(cla.id)} id={String(cla.title)} />
+                <Label htmlFor={String(cla.title)}>{cla.title}</Label>
+              </div>
+            ))}
+        </RadioGroup>
+      </CardContent>
+    </Card>
+  );
+};
+
+const Sizes = () => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((store) => store.filters.filters);
+  const filteredSizes = state.find((filter) => filter.name === "sizes");
+  const {
+    data: sizes,
+    isLoading: isLoadingSizes,
+    isError: isErrorSizes,
+  } = useGetSizesQuery({});
+  const handleChange = (value: string) => {
+    dispatch(
+      addFilter({
+        name: "sizes",
+        value:
+          // filteredSizes?.value !== "all" &&
+          filteredSizes?.value + String(value),
+      }),
+    );
+
+    const filters = Array.from(String(filteredSizes?.value)).slice(3);
+    console.log(filters.join(""));
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>القياس</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-3 space-x-2">
+            <Checkbox
+              id="all"
+              value={filteredSizes?.value}
+              checked={filteredSizes?.value === "all"}
+              onCheckedChange={() => handleChange("all")}
+            />
+            <label
+              htmlFor="all"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              الجميع
+            </label>
+          </div>
+          {(sizes !== null || sizes !== undefined) &&
+            sizes?.map((size) => (
+              <div className="flex items-center gap-3 space-x-2" key={size.id}>
+                <Checkbox
+                  id={String(size.size)}
+                  value={size.id}
+                  checked={filteredSizes?.value?.includes(String(size.id))}
+                  onCheckedChange={() => handleChange(String(size.id))}
+                />
+                <label
+                  htmlFor={String(size.size)}
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  {size.size}
+                </label>
+              </div>
+            ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+const Colors = () => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((store) => store.filters.filters);
+  const filteredColors = state.find((filter) => filter.name === "colors");
+  const {
+    data: colors,
+    isLoading: isLoadingColors,
+    isError: isErrorColors,
+  } = useGetColorsQuery({});
+  const handleChange = (value: string) => {
+    dispatch(addFilter({ name: "colors", value: String(value) }));
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>اللون</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-3 space-x-2">
+            <Checkbox id="all" checked={filteredColors?.value === "all"} />
+            <label
+              htmlFor="all"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              الجميع
+            </label>
+          </div>
+          {(colors !== null || colors !== undefined) &&
+            colors?.map((color) => (
+              <div className="flex items-center gap-3 space-x-2" key={color.id}>
+                <Checkbox
+                  id={String(color.title)}
+                  checked={filteredColors?.value?.includes(String(color.id))}
+                />
+                <label
+                  htmlFor={String(color.title)}
+                  className={`bg-${color.title} text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70`}
+                >
+                  {color.title}
+                </label>
+              </div>
+            ))}
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
--- lib/redux/slices/filters/filters-slice.ts
+++ lib/redux/slices/filters/filters-slice.ts
@@ -1,29 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 type Filter = {
-  id: number;
-  name: string;
+  name: "category" | "class" | "sizes" | "colors";
+  value: string | undefined;
 };
 
 export const filtersSlice = createSlice({
   name: "filters",
   initialState: {
-    filters: <Filter[]>[],
+    filters: <Filter[]>[
+      { name: "category", value: "all" },
+      { name: "class", value: "all" },
+      { name: "sizes", value: "all" },
+      { name: "colors", value: "all" },
+    ],
   },
   reducers: {
     addFilter: (state, action) => {
+      const exist = state.filters.find(
+        (filter: Filter) => filter.name === action.payload.name,
+      );
+      if (exist) {
+        const newArray = state.filters.map((cur: Filter) => {
+          if (cur.name === action.payload.name) {
+            cur.value = action.payload.value;
+          }
+          return cur;
+        });
+        state.filters = newArray;
+        return;
+      }
       state.filters = [...state.filters, action.payload];
     },
 
     deleteFilter: (state, action) => {
       const newArray = state.filters.filter(
-        (cur: Filter) => cur.id !== action.payload,
+        (cur: Filter) => cur.name !== action.payload,
       );
 
       state.filters = newArray;
       return;
     },
+    resetFilters: (state, action) => {
+      state.filters = [
+        { name: "category", value: "all" },
+        { name: "class", value: "all" },
+        { name: "sizes", value: "all" },
+        { name: "colors", value: "all" },
+      ];
+      return;
+    },
   },
 });
 
-export const { addFilter, deleteFilter } = filtersSlice.actions;
+export const { addFilter, deleteFilter, resetFilters } = filtersSlice.actions;
